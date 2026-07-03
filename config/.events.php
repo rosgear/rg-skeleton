582b5b9e9724d2bef79a0cf631b11a0bf8401ba2
Anton Tivonenko <rosgear.it@gmail.com>
2026-07-03 17:26:10 +0300

    Events

---
+++ config/.events.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Файл конфигурации слушателей событий (модулей, расширений модулей, виджетов).
+ * 
+ * Пример объявления событий слушателей:
+ * ```
+ * return [
+ *     'onFooBarEvent' => [
+ *         ['widgetId', 'widget'],
+ *         // ...
+ *         ['moduleId', 'module'],
+ *         // ...
+ *         ['extensionId', 'extension'],
+ *         // ...
+ *     ],
+ *     // ...
+ * ];
+ * ```
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+ 
+return [
+];
