0eb7880d39bf770c32ff62be68a7e0fd4b1b79d6
Anton Tivonenko <rosgear.it@gmail.com>
2026-07-03 17:30:52 +0300

    Services

---
+++ config/frontend/.services.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Файл конфигурации менеджера служб.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+ return [
+];
